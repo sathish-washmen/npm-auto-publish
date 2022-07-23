f5daa270336173e1bbab6c57c3f86cdfb347c5f4
Sathish <sathish.gajarla@washmen.com>
2022-07-23 23:19:48 +0400

    chore: add logs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-console.log('hello world!, Im testing the npm auto publishing');
+console.log('hello world!, Im testing the npm auto publishing...');
 console.warn('Adding the working to test auto publishing on pr merge')
